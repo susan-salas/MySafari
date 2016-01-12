77fc709d2f174580320d1ad351f67be57579ffc6
Susan Salas <SusanSalas@Susans-MacBook-Pro-2.local>
2016-01-12 13:55:53 -0800

    first commit

---
+++ MySafari/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MySafari
+//
+//  Created by Susan Salas on 1/12/16.
+//  Copyright (c) 2016 Susan Salas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MySafari/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MySafari
+//
+//  Created by Susan Salas on 1/12/16.
+//  Copyright (c) 2016 Susan Salas. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
