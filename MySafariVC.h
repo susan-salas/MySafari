bd94d72eaf2f72940049257dd684c47024097e5b
Rafael Auriemo <rafael.auriemo@gmail.com>
2016-01-12 14:02:59 -0800

    replaced default view controllers

---
+++ MySafariVC.h
@@ -0,0 +1,13 @@
+//
+//  MySafariVC.h
+//  MySafari
+//
+//  Created by Rafael Auriemo on 1/12/16.
+//  Copyright © 2016 Susan Salas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MySafariVC : UIViewController
+
+@end
---
+++ MySafariVC.m
@@ -0,0 +1,37 @@
+//
+//  MySafariVC.m
+//  MySafari
+//
+//  Created by Rafael Auriemo on 1/12/16.
+//  Copyright © 2016 Susan Salas. All rights reserved.
+//
+
+#import "MySafariVC.h"
+
+@interface MySafariVC ()
+
+@end
+
+@implementation MySafariVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
